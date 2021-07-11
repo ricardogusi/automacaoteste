4778a033ecd972a1d38bbbbb450b010ffdaa586a
Ricardo Gusi <ricardogusi@hotmail.com>
2021-07-11 15:54:57 -0300

    first

---
+++ src/main/java/gusi/springframework/springdata/controller/Ledcontroller.java
@@ -0,0 +1,24 @@
+package gusi.springframework.springdata.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.pi4j.io.gpio.GpioController;
+import com.pi4j.io.gpio.GpioFactory;
+import com.pi4j.io.gpio.GpioPinDigitalOutput;
+import com.pi4j.io.gpio.PinState;
+import com.pi4j.io.gpio.RaspiPin;
+
+public class Ledcontroller {
+	
+	private static GpioPinDigitalOutput pin;
+	
+	@RequestMapping("/light")
+	public String light() { 
+		if(pin ==null) { 
+			GpioController gpio = GpioFactory.getInstance();
+			pin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_01, "Led", PinState.LOW);
+		}
+		pin.toggle();
+		return "On";
+	}
+}
